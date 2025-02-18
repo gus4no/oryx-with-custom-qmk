beed3663722be5a76a8d3b5bc89cd214ee395fbe
Gustavo Robles <gus4no@users.noreply.github.com>
2025-02-18 12:56:22 -0600

    Update achordion.c

--- JJPLm/features/achordion.c
+++ JJPLm/features/achordion.c
@@ -335,6 +335,12 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
                                            keyrecord_t* tap_hold_record,
                                            uint16_t other_keycode,
                                            keyrecord_t* other_record) {
+  switch (tap_hold_keycode) {
+    case HOME_T:  // A + U.
+      if (other_keycode == HOME_R) { return true; }
+      break;
+  }
+  
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
