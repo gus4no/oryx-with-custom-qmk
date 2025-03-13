acada8dc1ab8897b93b0f69ea9d5dbb923941c68
Gustavo Robles <gustavo.robles@reverb.com>
2025-03-13 16:15:10 -0600

    inien

--- JJPLm/features/chordal.c
+++ JJPLm/features/chordal.c
@@ -15,3 +15,14 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
             return QUICK_TAP_TERM;
     }
 }
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1,KC_S):
+            // Immediately select the hold action when another key is pressed.
+            return true;
+        default:
+            // Do not select the hold action when another key is pressed.
+            return false;
+    }
+}
