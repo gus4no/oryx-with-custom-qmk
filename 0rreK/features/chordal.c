de13a1bfe854ea7baf16f5e3819fa5db1ff3838e
Gustavo Robles <gustavo.robles@reverb.com>
2025-03-12 21:58:49 -0600

    configure handednes

--- 0rreK/features/chordal.c
+++ 0rreK/features/chordal.c
@@ -4,5 +4,5 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                            'L', '*',  'R', 'R'
+                            'L', 'L',  'R', 'R'
     );
