863f388183acd15c95c79c99147b778c4d495b49
Gustavo Robles <gustavo.robles@reverb.com>
2025-03-13 15:38:16 -0600

    allow quick tap term per key

--- JJPLm/features/chordal.c
+++ JJPLm/features/chordal.c
@@ -6,3 +6,12 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
                             '*', '*',  '*', 'R'
     );
+
+uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LT(1,KC_S):
+            return 0;
+        default:
+            return QUICK_TAP_TERM;
+    }
+}
