393c0b06cdd6f8226f4dabe1ee15475c6c30495c
Gustavo Robles <gustavo.robles@reverb.com>
2025-03-14 08:52:58 -0600

    just enable quick tap term

--- JJPLm/features/chordal.c
+++ JJPLm/features/chordal.c
@@ -16,13 +16,13 @@ uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LT(1,KC_S):
-            // Immediately select the hold action when another key is pressed.
-            return true;
-        default:
-            // Do not select the hold action when another key is pressed.
-            return false;
-    }
-}
+//bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+//    switch (keycode) {
+//        case LT(1,KC_S):
+//            // Immediately select the hold action when another key is pressed.
+//            return true;
+//        default:
+//            // Do not select the hold action when another key is pressed.
+//            return false;
+//    }
+//}
