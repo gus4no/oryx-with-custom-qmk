17161ffb7b08d633b0646eb9652b03f1caffeeef
Gustavo Robles <gustavo.robles@reverb.com>
2025-03-20 13:46:54 -0600

    hien

--- 0rreK/features/chordal.c
+++ 0rreK/features/chordal.c
@@ -4,7 +4,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                            '*', 'L',  'R', '*'
+                            '*', '*',  'R', '*'
     );
 
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
