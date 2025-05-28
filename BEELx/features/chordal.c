995040e04961af4daab37bd4547b1780376d5363
Gustavo Robles <gustavo.robles@reverb.com>
2025-05-27 22:03:51 -0600

    test

--- BEELx/features/chordal.c
+++ BEELx/features/chordal.c
@@ -9,7 +9,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
 
 uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LT(1,KC_S):
+        case LT(1, KC_S):
             return 0;
         default:
             return QUICK_TAP_TERM;
